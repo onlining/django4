24c295aba5b935881017abcf0aed8204196848b0
gimuminjeong <ebsgreat7@gmail.com>
2022-01-27 12:38:18 +0900

    enter

--- django5/accounts/views.py
+++ django5/accounts/views.py
@@ -1,51 +1,53 @@
 from django.shortcuts import render,redirect
-from django.views import View
-from django.contrib.auth.models import User
-from django.contrib import auth
+from argon2 import PasswordHasher
 from .models import User
+from django.db import transaction
+from .forms import RegisterForm
 
 # Create your views here.
 
 
 def signup(request):
-    if request.method == 'GET':
-        return render(request, 'signup.html')
-    
-    elif request.method == 'POST':
-        user_id=request.POST.get('id','')
-        user_pw=request.POST.get('pw','')
-        user_pw_confirm=request.POST.get('pw-confirm','')
-        user_name=request.POST.get('name','')
-        user_email=request.POST.get('email','')
-
-        if (user_id or user_pw or user_pw_confirm or user_name or user_email)=='':
-            return redirect('/accounts/signup')
-        elif user_pw != user_pw_confirm:
-            return redirect('/accounts/signup')
-        else:
-            user = User(
-                user_id=user_id,
-                user_pw=user_pw,
-                user_name=user_name,
-                user_email=user_email
+    register_form=RegisterForm()
+    #지금까지작성한 RegsiterForm을 가져옵니다
+    context={'forms': register_form}
+    #아무런 값도 입력하지 않은 빈폼을 register_form이라는 변수에 할당합니다.context변수에 dict타입으로 register_form객체를 넣습니다.
+    if request.method =='GET':
+        return render(request, '/sign.html' , context)
+        #렌더링함수에 context를 전달합니다. 이 과정을 통해 render함수는 accounts/sign.html 파일을 렌더링할때 context에 대한 정보도 같이 전달하게 됩니다
+    elif request.method =='POST':
+        register_form=RegisterForm(request.POST)
+        if register_form.is_valid():
+            user=User(
+                user_id=register_form.user_id,
+                user_pw=register_form.user_pw,
+                user_name=register_form.user_name,
+                user_email =register_form.user_email
             )
             user.save()
-        return redirect('/login')
+            return redirect('/login.html')
+        else:
+            context['forms']=register_form
+            if register_form.errors:
+                for value in register_form.errors.values():
+                    context['error']=value
 
-
-
-
+        return render(request, '/sign.html',context)
 
 def login(request):
-    if request.method == 'POST':
-        username = request.POST['username']
-        password = request.POST['password']
-        user = authenticate(request, username=username, password=password)
-        if user is not None:
-            auth.login(request, user)
-            return redirect('django6:django5')
-        else:
-            return render(request, 'login.html', {'error': 'username or password is incorrect.'})
-    else:
-        return render(request, 'login.html')
+    loginform = LoginForm()
+    context={'forms': loginform}
+    if request.method=='GET':
+        return render(request, '/login.html',context)
 
+    elif request.method=='POST':
+        loginform=LoginForm(request.POST)
+
+        if loginform.is_valid():
+            return redirect('/')
+        else:
+            context['forms']=loginform
+            if loginform.errors:
+                for value in loginform.errors.values():
+                    context['error']=value
+        return render(request, '/login.html',context)
